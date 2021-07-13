61ea3b970a22ef20345d0d642e8efd96c8d972bf
Uptaker <markus.tammeoja@gmail.com>
2021-07-14 01:19:38 +0300

    Initial project add

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # pet-clinic-demo
+
 Recreating the famous Spring Pet Clinic demo, which showcases various Spring features connected to a robust database.
---
+++ src/test/java/codes/drinky/petclinicdemo/PetClinicDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package codes.drinky.petclinicdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
