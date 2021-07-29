a8d58f2e9b45beb44e93dfe028a426c1e6a5f13f
Uptaker <markus.tammeoja@gmail.com>
2021-07-30 02:05:23 +0300

    Added PetTypeService. Closes #27

---
+++ pet-clinic-data/src/main/java/codes/drinky/petclinicdemo/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package codes.drinky.petclinicdemo.services;
+
+import codes.drinky.petclinicdemo.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+
+}
---
+++ pet-clinic-data/src/main/java/codes/drinky/petclinicdemo/services/map/PetTypeMapService.java
@@ -0,0 +1,35 @@
+package codes.drinky.petclinicdemo.services.map;
+
+import codes.drinky.petclinicdemo.model.PetType;
+import codes.drinky.petclinicdemo.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
