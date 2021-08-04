01dfb3145d4ae1010175683d84dd535c730a1ddc
Uptaker <markus.tammeoja@gmail.com>
2021-08-04 20:06:09 +0300

    Update owner with JPA entity mappings. Closes #34

--- pet-clinic-data/src/main/java/codes/drinky/petclinicdemo/model/Person.java
+++ pet-clinic-data/src/main/java/codes/drinky/petclinicdemo/model/Person.java
@@ -1,8 +1,15 @@
 package codes.drinky.petclinicdemo.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity {
 
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name") // hibernate by default would do this anyway. camelCase to snake_case
     private String lastName;
 
     public String getFirstName() {
