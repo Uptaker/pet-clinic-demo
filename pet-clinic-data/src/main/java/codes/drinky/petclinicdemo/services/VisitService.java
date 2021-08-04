d5e1b4230e083f0f062b48f9e7890561d9e1d99b
Uptaker <markus.tammeoja@gmail.com>
2021-08-04 21:48:29 +0300

    Implement Map Impl for Visit. Closes #44

---
+++ pet-clinic-data/src/main/java/codes/drinky/petclinicdemo/services/VisitService.java
@@ -0,0 +1,6 @@
+package codes.drinky.petclinicdemo.services;
+
+import codes.drinky.petclinicdemo.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/codes/drinky/petclinicdemo/services/map/VisitMapService.java
@@ -0,0 +1,40 @@
+package codes.drinky.petclinicdemo.services.map;
+
+import codes.drinky.petclinicdemo.model.Visit;
+import codes.drinky.petclinicdemo.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+
+        if(object.getPet() == null || object.getPet().getOwner() == null || object.getPet().getId() == null ||
+            object.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+
+        return super.save(object);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
