2e5b8848bdd3b872a37846f488be5e975f41a950
Uptaker <markus.tammeoja@gmail.com>
2021-08-04 21:02:02 +0300

    Create Spring Data JPA service for vets. Closes #39

---
+++ pet-clinic-data/src/main/java/codes/drinky/petclinicdemo/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,57 @@
+package codes.drinky.petclinicdemo.services.springdatajpa;
+
+import codes.drinky.petclinicdemo.model.Vet;
+import codes.drinky.petclinicdemo.repositories.VetRepository;
+import codes.drinky.petclinicdemo.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+
+    private VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+
+        Set<Vet> vets = new HashSet<>();
+
+        vetRepository.findAll().forEach(vets::add);
+
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+
+        return vetRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        vetRepository.deleteById(aLong);
+    }
+
+    @Override
+    public Vet findByLastName(String lastName) {
+        return null;
+    }
+}
