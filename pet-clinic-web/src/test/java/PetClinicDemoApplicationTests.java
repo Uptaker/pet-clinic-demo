344d38d0249f45c88df803d0736c2d018e29791a
Uptaker <markus.tammeoja@gmail.com>
2021-08-11 13:37:57 +0300

    Add changes to dependencies to run jUnit5 tests

--- pet-clinic-web/src/test/java/PetClinicDemoApplicationTests.java
+++ pet-clinic-web/src/test/java/PetClinicDemoApplicationTests.java
@@ -1,7 +1,11 @@
+import codes.drinky.petclinicdemo.PetClinicDemoApplication;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = PetClinicDemoApplication.class)
 class PetClinicDemoApplicationTests {
 
     @Test
